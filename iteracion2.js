02f216d1dc338e7959b620447183eb367dfce29d
alvaroFGG <alvaro@mbp-de-alvaro.home>
2022-04-29 20:46:01 +0200

    segunda iteracion

---
+++ iteracion2.js
@@ -0,0 +1,41 @@
+const users = [
+    {name: 'Manolo el del bombo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 50},
+            rain: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'Mortadelo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 30},
+            shower: {format: 'ogg', volume: 55},
+            train: {format: 'mp3', volume: 60},
+        }
+    },
+    {name: 'Super Lopez',
+        favoritesSounds: {
+            shower: {format: 'mp3', volume: 50},
+            train: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'El culebra',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 67},
+            wind: {format: 'ogg', volume: 35},
+            firecamp: {format: 'mp3', volume: 60},
+        }
+    },
+];
+
+
+for(let i of users){
+    let sum = 0;
+    for(let j in i.favoritesSounds){
+        sum += i.favoritesSounds[j].volume;
+    }
+    let media = sum/3;
+    console.log('La media de volumen de: ',i.name,' es: ',media);
+}
+
